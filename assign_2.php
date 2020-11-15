d76cfa1288f250acad891a996877412968197a0e
Gillian Carballo <65936626+gilliancarballo@users.noreply.github.com>
2020-11-15 11:19:23 +0800

    Update assign_2.php

--- assign_2.php
+++ assign_2.php
@@ -6,12 +6,12 @@
         public $email;
         
 
-        function addFriend ($name){
+        function addFriend ($name, $username, $email){
             $this->name =$name;
                 echo "{$this->name} Added a Friend";
         }
 
-        function postStatus ($name){
+        function postStatus ($name, $username, $email){
             $this->name =$name;
                 echo "{$this->name}Posted a Status";
         }
@@ -20,6 +20,6 @@
        
     }
     $nameko = new User();
-    $nameko -> addFriend('Gillian');
-    $nameko -> postStatus ('Gillian');
+    $nameko -> addFriend('Gillian, gilliancarballo, gillian.carballo0319@gmail.com');
+    $nameko -> postStatus ('Gillian, gilliancarballo, gillian.carballo0319@gmail.com');
 ?>
